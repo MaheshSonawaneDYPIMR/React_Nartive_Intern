33bcab84f7350d45f475022eb1737cdf51a9abe3
Mahesh Sonawane <147mahesh258@gmail.com>
2023-12-30 00:01:09 +0530

    card behaving good

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,12 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import Card from "./src/components/Card";
+import {
+  moderateScale,
+  moderateVerticalScale,
+} from "react-native-size-matters";
+
+const colors = ["#8ecae6" , "#6fffe9" , "#e9c46a" , "#dda15e" , "#bc6c25"]; // Dummy color data
 
 export default function App() {
+  const [stack, setStack] = useState(
+    colors.map((color, index) => ({
+      color,
+      index,
+      stackPosition: colors.length,
+    }))
+  );
+
+  const handleSwipe = (direction, index) => {
+    setStack((prevStack) => {
+      const updatedStack = prevStack.filter((_, i) => i !== index);
+      return updatedStack.map((card, i) => ({
+        ...card,
+        stackPosition: updatedStack.length - i,
+      }));
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.cardsContainer}>
+        {stack.map((card, index) => (
+          <Card
+            key={card.color}
+            color={card.color}
+            index={index}
+            onSwipe={handleSwipe}
+            stackPosition={card.stackPosition}
+          />
+        ))}
+      </View>
     </View>
   );
 }
@@ -14,8 +47,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardsContainer: {
+    width: moderateScale(325),
+    height: moderateVerticalScale(500),
   },
 });
--- reactApp/src/components/Card.js
+++ reactApp/src/components/Card.js
@@ -1,14 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef } from "react";
+import { View, Animated, PanResponder, StyleSheet } from "react-native";
+import {
+  moderateScale,
+  moderateVerticalScale,
+} from "react-native-size-matters";
 
-const Card = () => {
+
+
+const Card = ({ color, onSwipe, index, stackPosition }) => {
+  const pan = useRef(new Animated.ValueXY()).current;
+  
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, gesture) => {
+      pan.setValue({ x: gesture.dx, y: gesture.dy });
+    },
+
+    onPanResponderRelease: (_, gesture) => {
+      if (gesture.dx > 120) {
+        onSwipe("right", index);
+      } else if (gesture.dx < -120) {
+        onSwipe("left", index);
+      } else {
+        Animated.spring(pan, {
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
+          speed: 10,
+          bounciness,
+        }).start();
+      }
+    },
+  });
+
+  const width = moderateScale(325) - index * 10;
+  const height = moderateScale(500); // Adjust width based on index
+
+  const animatedStyle = {
+    transform: [
+      { translateX: pan.x },
+      { translateY: pan.y },
+      {
+        rotate: pan.x.interpolate({
+          inputRange: [-200, 0, 200],
+          outputRange: ["-10deg", "0deg", "10deg"],
+        }),
+      },
+    ],
+
+    zIndex: stackPosition - index,
+    top: index < 3 ? index * 10 : undefined, // Conditionally set top based on index
+    height: index > 2 ? moderateScale(0) : moderateScale(500),
+    width,
+    alignSelf: "center",
+    // Update the zIndex to reposition cards in the stack
+  };
+
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
-  )
-}
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={[styles.card, animatedStyle, { backgroundColor: color }]}
+    ></Animated.View>
+  );
+};
 
-export default Card
+const styles = StyleSheet.create({
+  card: {
+    position: "absolute",
+    width: moderateScale(325),
+    height: moderateVerticalScale(500),
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Card;
